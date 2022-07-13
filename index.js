854f3d52abaa8c31a193920c91ffe8544d981d9e
Karabinskiy13 <ivan.karabisnkiy1308@gmail.com>
2022-07-13 21:11:41 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,59 @@
+let featuredImage = document.getElementsByClassName('featured-image')[0]
+fetch('data.json')
+    .then(value => value.json())
+    .then(value => {
+        console.log(value)
+        for (const divElement of value.slice(1, 7)) {
+            if (`${divElement.rating}` >= 4) {
+                featuredImage.innerHTML += `
+                    <div class='imageFromJSON'>
+<svg viewBox="0 0 576 512" xmlns="http://www.w3.org/2000/svg"><path d="M259.3 17.8L194 150.2 47.9 171.5c-26.2 3.8-36.7 36.1-17.7 54.6l105.7 103-25 145.5c-4.5 26.3 23.2 46 46.4 33.7L288 439.6l130.7 68.7c23.2 12.2 50.9-7.4 46.4-33.7l-25-145.5 105.7-103c19-18.5 8.5-50.8-17.7-54.6L382 150.2 316.7 17.8c-11.7-23.6-45.6-23.9-57.4 0z"></path></svg>
+                        <img src ="${divElement.image}" alt="" class = 'single-image'/>
+                        <div class='featured-image-info'>
+                        <h2 class = 'titleFromJSON'>${divElement.title}</h2>
+                            <p class='tags'>${divElement.tags}</p>
+                        </div>
+                    </div>
+                `
+            }
+        }
+    })
+
+let lastImage = document.getElementsByClassName('last-image')[0]
+fetch('data.json')
+    .then(value => value.json())
+    .then(value => {
+        console.log(value)
+        for (const divElement of value.slice(0, 2)) {
+            if (`${divElement.age}` <= 5) {
+                lastImage.innerHTML += `
+                    <div class='imageFromJSON'>
+<svg viewBox="0 0 576 512" xmlns="http://www.w3.org/2000/svg"><path d="M528.1 171.5L382 150.2 316.7 17.8c-11.7-23.6-45.6-23.9-57.4 0L194 150.2 47.9 171.5c-26.2 3.8-36.7 36.1-17.7 54.6l105.7 103-25 145.5c-4.5 26.3 23.2 46 46.4 33.7L288 439.6l130.7 68.7c23.2 12.2 50.9-7.4 46.4-33.7l-25-145.5 105.7-103c19-18.5 8.5-50.8-17.7-54.6zM388.6 312.3l23.7 138.4L288 385.4l-124.3 65.3 23.7-138.4-100.6-98 139-20.2 62.2-126 62.2 126 139 20.2-100.6 98z"></path></svg>
+                        <img src ="${divElement.image}" alt="" class = 'single-image'/>
+                        <div class='featured-image-info'>
+                        <h2 class = 'titleFromJSON'>${divElement.title}</h2>
+                            <p class='tags'>${divElement.tags}</p>
+                        </div>
+                    </div>
+                `
+            }
+        }
+    })
+
+let numOfElementsInDom = document.getElementsByTagName('*').length
+const arrOfTags = document.getElementsByTagName('*');
+
+let groupByNameTags = []
+let groupByLength = []
+for (let i = 0; i < arrOfTags.length; i++) {
+    let arrOfTag = arrOfTags[i]
+    groupByNameTags.push(document.getElementsByTagName(arrOfTag.tagName));
+    if (groupByLength[arrOfTag.tagName.length] === undefined) {
+        groupByLength[arrOfTag.tagName.length] = [];
+    }
+    groupByLength[arrOfTag.tagName.length].push(arrOfTag.tagName);
+}
+
+console.log(numOfElementsInDom, groupByNameTags, groupByLength)
+
+
